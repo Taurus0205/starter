f3364c67512a1ff07345f2904053100364b6ffc6
Taurus0205 <dolchin1990@mail.ru>
2021-06-26 08:10:45 +0500

    Remove all unnecessary files form the starter

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 const elHumbergerBtn = document.querySelector(".hamburger-btn");
 const elHeader = document.querySelector(".header");
 
-const elFaqsBtn = document.querySelector(".faqs__btn");
-const elFaqsItem = document.querySelector(".faqs__item");
-
 elHumbergerBtn.addEventListener("click", function () {
   elHeader.classList.toggle("header--adoptive");
-});
-
-elFaqsBtn.addEventListener("click", function () {
-  elFaqsItem.classList.toggle("faqs__item--active");
 });
